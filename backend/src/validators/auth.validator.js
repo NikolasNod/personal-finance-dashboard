1c77d28d0d0f555154ab889c2dbdda9fd48ec508
NikolasNod <nikolas.nodar@gmail.com>
2026-09-09 17:57:09 +0300

    Create loginSchema in auth validator

--- backend/src/validators/auth.validator.js
+++ backend/src/validators/auth.validator.js
@@ -1,16 +1,18 @@
 const { z } = require("zod");
 
 const registerSchema = z.object({
-    email: z
-        .string()
-        .email("Invalid email address"),
+  email: z.string().email("Invalid email address"),
 
-    password: z
-        .string()
-        .min(6, "Password must be at least 6 characters")
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
 
+const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+
+  password: z.string().min(1, "Password is required"),
 });
 
 module.exports = {
-    registerSchema
+  registerSchema,
+  loginSchema,
 };
