d6999e7fee564bd997c179220a3653cb55c42237
NikolasNod <nikolas.nodar@gmail.com>
2026-09-12 16:11:35 +0300

    Fix auth.controller missing requirements

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const { registerSchema, loginSchema } = require("../validators/auth.validator");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const authController = {
   async login(req, res) {
